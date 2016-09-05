97fdfc547d49c24a2ca7ddb424a621f90f7b30f6
dchad <dgchadwick@outlook.com>
2016-09-05 20:11:57 +0800

    Feature Extraction Updates.

---
+++ vs/feature_extraction_javascript.py
@@ -0,0 +1,31 @@
+# feature_extraction_javascript.py
+#
+# Read a list of Javascript files and extract
+# feature sets from them.
+#
+# Input:
+#
+# Output:
+#
+# Author: Derek Chadwick
+# Date  : 05/09/2016
+#
+# TODO: all of the things
+
+from multiprocessing import Pool
+import os
+from csv import writer
+import numpy as np
+import pandas as pd
+import math
+import scipy.misc
+import array
+import time as tm
+import re
+import subprocess as sub
+
+
+# Start of Script
+
+
+# End of Script
